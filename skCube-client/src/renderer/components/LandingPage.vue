6815d87b5a121da5ceb7866a82e1b644de9e1b63
Matej Nemcek <ybdaba@gmail.com>
2017-07-19 21:54:40 +0200

    choosing folder for gsr and dialog

--- skCube-client/src/renderer/components/LandingPage.vue
+++ skCube-client/src/renderer/components/LandingPage.vue
@@ -1,114 +1,128 @@
 <template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/sk-cube-rr.png" alt="skcube">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to skCube client GSR collect0r
-        </span>
-        <system-information></system-information>
-      </div>
+<div id="wrapper">
+      <img id="logo" src="~@/assets/sk-cube-rr.png" alt="skcube">
+      <main>
+            <div class="left-side">
+                  <span class="title">
+                        skCube client GSR collect0r
+                  </span>
+                  <system-information></system-information>
+            </div>
 
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            Select your folder where gsr files are created and we will do the rest
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-          <input type="file" webkitdirectory />
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
-  </div>
+            <div class="right-side">
+                  <div class="doc">
+                        <div class="title">Getting Started</div>
+                        <p>
+                              Select your folder where GSR files are created and we will do the rest
+                        </p>
+                        <button v-on:click="openDialog">Choose folder</button>
+                        <br>
+                        <br>
+                        <p>
+                              Selected path:
+                        </p>
+                        <br>
+                  </div>
+            </div>
+      </main>
+</div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
+import SystemInformation from './LandingPage/SystemInformation'
 
-  export default {
-    name: 'landing-page',
-    components: { SystemInformation },
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
-      }
+export default {
+  name: 'landing-page',
+  components: {
+    SystemInformation
+  },
+  methods: {
+    open(link) {
+      this.$electron.shell.openExternal(link)
+    },
+    openDialog: function () {
+      const {
+        dialog
+      } = require('electron').remote
+      const path = dialog.showOpenDialog({
+        filters: [{
+          name: 'Packets',
+          extensions: ['gsr']
+        }],
+        properties: ['openDirectory']
+      })
+      console.log(path)
     }
   }
-
+}
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 
-  * {
+* {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-  }
+}
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
+body {
+    font-family: 'Source Sans Pro', sans-serif;
+}
 
-  #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
+#wrapper {
+    background: radial-gradient( ellipse at top left,
+    rgba(255, 255, 255, 1) 40%,
+    rgba(229, 229, 229, .9) 100%);
     height: 100vh;
     padding: 60px 80px;
     width: 100vw;
-  }
+}
 
-  #logo {
+#logo {
     height: auto;
     margin-bottom: 20px;
     width: 200px;
-  }
+}
 
-  main {
+main {
     display: flex;
     justify-content: space-between;
-  }
+}
 
-  main > div { flex-basis: 50%; }
+main>div {
+    flex-basis: 50%;
+}
 
-  .left-side {
+.left-side {
     display: flex;
     flex-direction: column;
-  }
+}
 
-  .welcome {
+.welcome {
     color: #555;
     font-size: 23px;
     margin-bottom: 10px;
-  }
+}
 
-  .title {
+.title {
     color: #2c3e50;
     font-size: 20px;
     font-weight: bold;
     margin-bottom: 6px;
-  }
+}
 
-  .title.alt {
+.title.alt {
     font-size: 18px;
     margin-bottom: 10px;
-  }
+}
 
-  .doc p {
+.doc p {
     color: black;
     margin-bottom: 10px;
-  }
+}
 
-  .doc button {
+.doc button {
     font-size: .8em;
     cursor: pointer;
     outline: none;
@@ -120,10 +134,10 @@
     transition: all 0.15s ease;
     box-sizing: border-box;
     border: 1px solid #4fc08d;
-  }
+}
 
-  .doc button.alt {
+.doc button.alt {
     color: #42b983;
     background-color: transparent;
-  }
+}
 </style>
