6d50d33b9e708bbe3527dc3ab4a98887a25ef0dd
Matej Nemcek <ybdaba@gmail.com>
2017-07-26 21:49:25 +0200

    cors for server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const CWD = process.cwd()
 const config = require(CWD + '/config.json')
 
 var express = require('express')
+var cors = require('cors')
 var path = require('path')
 // var favicon = require('serve-favicon')
 var logger = require('morgan')
@@ -23,6 +24,8 @@ app.set('view engine', 'pug')
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
+app.use(cors())
+app.options('*', cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
