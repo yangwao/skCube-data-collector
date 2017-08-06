14e58886fc654007bd534fda8418057463235907
Matej Nemcek <ybdaba@gmail.com>
2017-08-06 15:52:47 +0200

    log verbose errors with ip

--- server/routes/index.js
+++ server/routes/index.js
@@ -47,29 +47,37 @@ router.get('/v1/createdAt/:epochTimeMs', function (req, res, next) {
 
 router.post('/v1/raw', upload.single('gsr'), function (req, res, next) {
   const b = req.body
-
+  l.info(req.file.mimetype)
   if (!req.file) {
+    l.warn(`${req.clientIp} - gsr missing`)
     return res.status(400).json({error: 'missing gsr attachment'})
   }
   if (req.file.encoding !== '7bit') {
+    l.warn(`${req.clientIp} - wrong encoding`)
     return res.status(400).json({error: 'gsr wrong encoding'})
   }
   if (req.file.mimetype !== 'text/plain') {
+    l.warn(`${req.clientIp} - wrong mimetype`)
     return res.status(400).json({error: 'gsr wrong mimetype'})
   }
   if (req.file.originalname.length > config.gsr.file.maxLength) {
+    l.warn(`${req.clientIp} - file originalname too long`)
     return res.status(400).json({error: 'gsr file originalname too long'})
   }
   if (req.file.size > config.gsr.file.maxSize) {
+    l.warn(`${req.clientIp} - file too big`)
     return res.status(400).json({error: 'gsr file too big'})
   }
   if (!b.sourceCallsign || b.sourceCallsign.length > config.limits.callsign.source) {
+    l.warn(`${req.clientIp} - sourceCallsign missing or too long`)
     return res.status(400).json({error: 'missing sourceCallsign or too long'})
   }
   if (!b.destinationCallsign || b.destinationCallsign.length > config.limits.callsign.destination) {
+    l.warn(`${req.clientIp} - destinationCallsign missing or too long`)
     return res.status(400).json({error: 'missing destinationCallsign or too long'})
   }
   if (!b.meta || b.meta.length > config.limits.meta) {
+    l.warn(`${req.clientIp} - meta missing or too long`)
     return res.status(400).json({error: 'missing meta or too long'})
   }
 
