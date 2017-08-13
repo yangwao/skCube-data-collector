ac00fa20bc27a1e633d22c47ca0cd145c591ad1b
Ivkaa <dark.wolfie184@gmail.com>
2017-08-13 17:41:58 +0200

    Add export to function that parses gsr data

--- parser/decode.ts
+++ parser/decode.ts
@@ -5,111 +5,110 @@
 import { COMStructure, COMObj, ADCSStructure, ADCSObj, PWRStructure, PWRObj, CDHSStructure, CDHSObj } from './data-structure';
 
 // Make sure we got a filename on the command line.
-if (process.argv.length !== 3) {
-  console.log('Please enter data as 3rd argument');
-  process.exit(1);
-}
+// if (process.argv.length !== 3) {
+//   console.log('Please enter data as 3rd argument');
+//   process.exit(1);
+// }
 
-const rawData = process.argv[2];
+// const rawData = process.argv[2];
 
-// TODO: add test for head data to filter out wrong packets
-const data = cleanUnwantedData(rawData);
-const type = findType(data);
-const newDataWithoutType = removeType(data);
-const newData = removeNewLines(newDataWithoutType);
+// export type Type = [
+//     number,
+//     string
+// ];
 
-export type Type = [
-    number,
-    string
-];
+// console.log(parseGsrData(rawData));
 
-switch (type) {
-    case 'CDHS':
-        console.log(parseByStructure(newData, CDHSStructure, CDHSObj));
-        break;
-    case 'ADCS':
-        console.log(parseByStructure(newData, ADCSStructure, ADCSObj));
-        break;
-    case 'COM':
-        console.log(parseByStructure(newData, COMStructure, COMObj));
-        break;
-    case 'PWR':
-        console.log(parseByStructure(newData, PWRStructure, PWRObj));
-        break;
-    case 'CAM':
-        throw new Error('Not supported type');
-    case 'EXP':
-        throw new Error('Not supported type');
-}
+export function parseGsrData(rawData: string) {
+    // TODO: add test for head data to filter out wrong packets
+    const data = cleanUnwantedData(rawData);
+    const type = findType(data);
+    const newDataWithoutType = removeType(data);
+    const newData = removeNewLines(newDataWithoutType);
 
-function cleanUnwantedData(rawData: string) {
-    const newData = rawData.slice(36);
-    return newData;
-}
-
-function findType(data: string) {
-    const n = data.slice(0, 2);
-    switch (n) {
-        case '01':
-            return 'CDHS';
-        case '02':
-            return 'ADCS';
-        case '03':
-            return 'COM';
-        case '04':
-            return 'CAM';
-        case '05':
-            return 'PWR';
-        case '06':
-            return 'EXP';
-        default:
-            throw new Error('invalid type');
+    switch (type) {
+        case 'CDHS':
+            return parseByStructure(newData, CDHSStructure, CDHSObj);
+        case 'ADCS':
+            return parseByStructure(newData, ADCSStructure, ADCSObj);
+        case 'COM':
+            return parseByStructure(newData, COMStructure, COMObj);
+        case 'PWR':
+            return parseByStructure(newData, PWRStructure, PWRObj);
+        case 'CAM':
+            throw new Error('CAM is not supported type');
+        case 'EXP':
+            throw new Error('EXP is not supported type');
     }
-}
 
-function removeType(data: string) {
-    return data.slice(2);
-}
+    function cleanUnwantedData(rawData: string) {
+        const newData = rawData.slice(36);
+        return newData;
+    }
 
-function removeNewLines(data: string) {
-    let newData = '';
-    for (const c of data) {
-        if (c !== '\n') {
-            newData += c
+    function findType(data: string) {
+        const n = data.slice(0, 2);
+        switch (n) {
+            case '01':
+                return 'CDHS';
+            case '02':
+                return 'ADCS';
+            case '03':
+                return 'COM';
+            case '04':
+                return 'CAM';
+            case '05':
+                return 'PWR';
+            case '06':
+                return 'EXP';
+            default:
+                throw new Error('invalid type');
         }
     }
-    return newData;
-}
 
-function parseByStructure(data: string, structure: Type[], obj: object) {
-    const resultArray: any = [];
-    let result: number = null;
+    function removeType(data: string) {
+        return data.slice(2);
+    }
 
-    structure.reduce((sum, s) => {
-        let hexNum = data.slice(sum, sum+s[0]);
-        console.log(hexNum)
-
-        // ~~~switch endianness~~~
-        const hexNumLittleEndian = hexNum.match(/../g).reverse().join('');
-
-        let num = parseInt(hexNumLittleEndian, 16);
-
-        if (s[1] === 'signed') {
-            const maxVal = Math.pow(2, hexNum.length / 2 * 8);
-            if (num > maxVal / 2 - 1) {
-                num = num - maxVal
+    function removeNewLines(data: string) {
+        let newData = '';
+        for (const c of data) {
+            if (c !== '\n') {
+                newData += c
             }
         }
-
-        resultArray.push(num);
-
-        result = sum+s[0];
-        return sum+s[0];
-    }, 0);
-
-    let i = 0;
-    for (let key in obj) {
-        obj[key] = resultArray[i++];
+        return newData;
     }
-    return obj;
+
+    function parseByStructure(data: string, structure: Type[], obj: object) {
+        const resultArray: any = [];
+        let result: number = null;
+
+        structure.reduce((sum, s) => {
+            let hexNum = data.slice(sum, sum+s[0]);
+
+            // ~~~switch endianness~~~
+            const hexNumLittleEndian = hexNum.match(/../g).reverse().join('');
+
+            let num = parseInt(hexNumLittleEndian, 16);
+
+            if (s[1] === 'signed') {
+                const maxVal = Math.pow(2, hexNum.length / 2 * 8);
+                if (num > maxVal / 2 - 1) {
+                    num = num - maxVal
+                }
+            }
+
+            resultArray.push(num);
+
+            result = sum+s[0];
+            return sum+s[0];
+        }, 0);
+
+        let i = 0;
+        for (let key in obj) {
+            obj[key] = resultArray[i++];
+        }
+        return obj;
+    }
 }
